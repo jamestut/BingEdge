c083a81f2fc2c7973ee0750e331088d6683b65e9
James <aslan.jnn@gmail.com>
2023-05-22 23:31:56 +1000

    Update description and version

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 const youtubeUrls = ["https://bing.com/*", "https://*.bing.com/*"];
-const desiredUA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41";
+const desiredUA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50";
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function (reqInfo) {
