3f1b9119ae81552d29998fea191b39d7a9bfaa3d
James <aslan.jnn@gmail.com>
2023-07-18 16:00:50 +1000

    Update variable name

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-const youtubeUrls = ["https://bing.com/*", "https://*.bing.com/*"];
+const bingUrls = ["https://bing.com/*", "https://*.bing.com/*"];
 const desiredUA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.50";
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function (reqInfo) {
     reqInfo.requestHeaders.push({ name:"User-Agent", value:desiredUA });
     return { requestHeaders: reqInfo.requestHeaders };
-  }, { urls: youtubeUrls }, ['blocking', 'requestHeaders']);
+  }, { urls: bingUrls }, ['blocking', 'requestHeaders']);
